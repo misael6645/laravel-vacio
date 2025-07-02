5adaff4719f57530be33a35ed0639e05475d39d7
misaelzelada <misael661001@gmail.com>
2025-07-02 17:58:21 -0400

    last commit 4

---
+++ resources/views/front/sign-in.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Hello, world!</title>
+</head>
+<body>
+<a class="btn btn-success" href="{{ route('microsoft.oAuth') }}">Sign In</a>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
