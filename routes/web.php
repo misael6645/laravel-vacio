b01ff68673ae3be035c58dbdc37f09d3821398d7
misaelzelada <misael661001@gmail.com>
2025-07-03 12:19:02 -0400

    update routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::get('/',[MicrosoftAuthController::class,'signInForm'])->name('sign.in');
 Route::get('/not-found',[MicrosoftAuthController::class,'notFound'])->name('not.found');
-Route::get('microsoft-oAuth',[MicrosoftAuthController::class,'microsoftOAuth'])->name('microsoft.oAuth');
-Route::get('auth/azure/callback',[MicrosoftAuthController::class,'microsoftOAuthCallback'])->name('microsoft.oAuth.callback');
+Route::get('/microsoft-oAuth',[MicrosoftAuthController::class,'microsoftOAuth'])->name('microsoft.oAuth');
+Route::get('/auth/azure/callback',[MicrosoftAuthController::class,'microsoftOAuthCallback'])->name('microsoft.oAuth.callback');
 Route::get('/dashboard', [MicrosoftAuthController::class, 'dashboard'])->name('dashboard');
 Route::get('/logout', [MicrosoftAuthController::class, 'logout'])->name('logout');
